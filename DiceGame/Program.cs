b20c013f58c74496713706d6b6ee7b41757cce04
Christopher Seaver <cseaver@tktk.ee>
2022-02-05 14:52:15 +0200

    MoreLoops

---
+++ DiceGame/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int cpuScore = 0;
+            int userScore = 0;
+
+            for(int i = 0; i < 3; i++)
+            {
+                int cpuRandom = rnd.Next(1, 7);
+
+                int userRandom = rnd.Next(1, 7);
+
+                Console.WriteLine($"Arvuti viskas {cpuRandom}. Kasutaja viaskas {userRandom}.");
+
+                if (cpuRandom < userRandom)
+                {
+                    Console.WriteLine("Kasutaja võitis raundi.");
+                    userScore = userScore + 1;
+                }
+                else if (cpuRandom > userRandom)
+                {
+                    Console.WriteLine("Arvuti võitis raundi.");
+                    cpuScore = cpuScore + 1;
+                }
+                else
+                {
+                    Console.WriteLine("Viik!");
+                }
+            }
+            if (cpuScore < userScore)
+            {
+                Console.WriteLine($"Kasutaja võitis mängu {userScore} punktiga.");
+            }
+            else if (cpuScore > userScore)
+            {
+                Console.WriteLine($"Arvuti on mängu võitnud {cpuScore} punktiga.");
+            }
+            else
+            {
+                Console.WriteLine("Mäng lõppes viigiga.");
+            }
+        }
+    }
+}
---
+++ LoginValidation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoginValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 3)
+            {
+
+                Console.WriteLine("Sisesta kasutajatunnus:");
+                string userName = Console.ReadLine();
+                Console.WriteLine("Sisesta salasõna:");
+                string userPassword = Console.ReadLine();
+
+                if (userName != "admin" || userPassword != "admin1234")
+                {
+                    i++;
+                    Console.WriteLine("Vale kasutajatunnus või salasõna. Proovi uuesti.");
+                    Console.WriteLine($"Katseid on alles {3 - i}.");
+                }
+                else
+                {
+                    Console.WriteLine("Tere tulemast!");
+                    break;
+                }
+            }
+        }
+    }
+}
